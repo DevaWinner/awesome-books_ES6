7a93bbe074d4d5a7357cdd9ba7b61d9198dc8b62
DevaWinner <winneraaniekan@gmail.com>
2023-06-08 15:48:17 +0100

    Add Book manager module and export

---
+++ modules/BookManager.js
@@ -0,0 +1,24 @@
+
+import bookCollection from './BookCollection.js';
+
+function displayBooks() {
+  const bookList = document.getElementById('bookList');
+  bookList.innerHTML = '';
+
+  bookCollection.getBooks().forEach((book, index) => {
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book');
+    bookElement.innerHTML = `
+      <div class="book-info">
+        <span>"${book.title}"</span>
+        <span> by ${book.author}</span>
+      </div>
+      <div class="btn-container">
+        <button class="remove-button">Remove</button>
+      </div>
+    `;
+    bookList.appendChild(bookElement);
+  });
+}
+
+export { displayBooks };
